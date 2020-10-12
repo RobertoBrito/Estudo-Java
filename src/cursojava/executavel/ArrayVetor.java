b7ac37198204c1c74f41f49c1d7a4d9829a96eb7
Roberto Brito <roberto.brito.oliveira@hotmail.com>
2020-10-12 12:38:24 -0300

    Criado uma classe Array básico para compreendo como funciona

---
+++ src/cursojava/executavel/ArrayVetor.java
@@ -0,0 +1,22 @@
+package cursojava.executavel;
+
+public class ArrayVetor {
+	/* usado para executar código */
+	public static void main(String[] args) {
+
+		/* Array pode ser de todos os tipos de dados e objeto também */
+
+		/* array sempre deve ter a quantidade de posições defenidas */
+		double[] notas = new double[4];
+
+		/* trabalhar com as posições */
+		notas[0] = 9.8;
+		notas[1] = 7.8;
+		notas[2] = 9.7;
+		notas[3] = 8.8;
+		
+		
+		System.out.println("");
+
+	}
+}
